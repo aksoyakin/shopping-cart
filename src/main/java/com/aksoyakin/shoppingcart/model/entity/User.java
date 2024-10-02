60516b2cf3862a0e13836d374cccd2bd5a1dbb9b
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-10-02 20:27:56 +0300

    user entity and its relations

---
+++ src/main/java/com/aksoyakin/shoppingcart/model/entity/User.java
@@ -0,0 +1,35 @@
+package com.aksoyakin.shoppingcart.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Cart cart;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> order;
+}
